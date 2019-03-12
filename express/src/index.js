e2100e647327686e07b2c6e01a3f1e6f9931dbb3
WindzLord <kk.outplayed@gmail.com>
2019-03-12 18:00:05 +0700

    ADD SERVER GRAPHQL

--- express/src/index.js
+++ express/src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
+import {ApolloProvider} from 'react-apollo';
+import {ApolloClient} from 'apollo-client';
+import {createHttpLink} from 'apollo-link-http';
+import {InMemoryCache} from 'apollo-cache-inmemory';
 import App from 'containers/App';
 import 'antd/dist/antd.css';
 import 'antd.css';
 import 'index.css';
 
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </BrowserRouter>,
   document.getElementById('root'),
 );
