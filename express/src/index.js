7ab495a343ba9579ddbe309c2fd6fa6a67cdecbe
WindzLord <kk.outplayed@gmail.com>
2019-03-13 10:24:20 +0700

    Revert "demo graphql"
    This reverts commit 58b723392e10c43b5ed9dc3edb605e731e665276.

--- express/src/index.js
+++ express/src/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import {ApolloProvider} from 'react-apollo';
-import {client} from 'settings/ApolloClient';
+import {ApolloClient} from 'apollo-client';
+import {createHttpLink} from 'apollo-link-http';
+import {InMemoryCache} from 'apollo-cache-inmemory';
 import App from 'containers/App';
 import 'antd/dist/antd.css';
 import 'antd.css';
 import 'index.css';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <BrowserRouter>
--- express/src/super/Home.js
+++ express/src/super/Home.js
@@ -1,32 +1,4 @@
 import React, {Component, Fragment} from 'react';
-import {Row, Col, Form, Input, Button} from 'antd';
-import LinkService from 'services/LinkService';
-import {Query, } from 'react-apollo';
-import gql from 'graphql-tag';
-
-const FEED_QUERY = gql`
-  {
-    feed {
-      links {
-        id
-        createdAt
-        url
-        description
-      }
-    }
-  }
-`;
-
-const POST_MUTATION = gql`
-  mutation PostMutation($description: String!, $url: String!) {
-    post(description: $description, url: $url) {
-      id
-      createdAt
-      url
-      description
-    }
-  }
-`;
 
 class Router extends Component {
   constructor() {
@@ -34,47 +6,9 @@ class Router extends Component {
     this.state = {};
   }
 
-  componentDidMount = () => {
-    LinkService.feed().then((response) => {
-      console.log(response);
-    });
-  };
-
   render() {
-    // const {getFieldDecorator} = this.props.form;
-
-    return (
-      <Fragment>
-        Home
-        <Query query={FEED_QUERY}>
-          {({loading, error, data}) => {
-            if (loading) return <div>Fetching</div>;
-            if (error) return <div>Error</div>;
-            return (
-              <div>
-                {data.feed.links.map((link) => (
-                  <div key={link.id}>{link.url}</div>
-                ))}
-              </div>
-            );
-          }}
-        </Query>
-        {/* <Form>
-          <Row>
-            <Col>
-              <Form.Item label='URL'>{getFieldDecorator('url')(<Input />)}</Form.Item>
-              <Form.Item label='Description'>{getFieldDecorator('description')(<Input />)}</Form.Item>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Button>Post</Button>
-            </Col>
-          </Row>
-        </Form> */}
-      </Fragment>
-    );
+    return <Fragment>Home</Fragment>;
   }
 }
 
-export default Form.create(Router);
+export default Router;
