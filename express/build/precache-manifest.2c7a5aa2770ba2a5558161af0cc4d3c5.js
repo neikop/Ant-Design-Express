a240c49a8f18c32a047267b94965e5da9dd05051
Khac Kien <kien.nk@teko.vn>
2019-04-05 14:11:53 +0700

    add build

---
+++ express/build/precache-manifest.2c7a5aa2770ba2a5558161af0cc4d3c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "eaec0af1ae6c2dc0b6be",
+    "url": "/static/js/main.7a738fc2.chunk.js"
+  },
+  {
+    "revision": "bf9c364aa14315275e32",
+    "url": "/static/js/2.3a2d2b9b.chunk.js"
+  },
+  {
+    "revision": "eaec0af1ae6c2dc0b6be",
+    "url": "/static/css/main.779e0001.chunk.css"
+  },
+  {
+    "revision": "bf9c364aa14315275e32",
+    "url": "/static/css/2.15df1874.chunk.css"
+  },
+  {
+    "revision": "02e943a258029d0e77f0f31858e35cc3",
+    "url": "/index.html"
+  }
+];
